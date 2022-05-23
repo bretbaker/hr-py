3058de7cb0f57a055cd9be3a67c807afe8e7abbd
Bret Baker <bretbaker808@gmail.com>
2022-05-23 16:34:19 -0400

    add first else if challenge

---
+++ ElseIf.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+n = 7
+
+if n % 2 == 0:
+    if n in range(2, 6):
+        print("Not Weird")
+
+    elif n in range(6, 21):
+        print("Weird")
+
+    elif n > 20:
+        print("Not Weird")
+else:
+    print("Weird")
